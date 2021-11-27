8413704cd7ea38e2dafcf3c2a8d54978a664455a
Hasim <Hasims2001@gmail.com>
2021-11-27 16:02:25 +0530

    Log in page + sign up page

--- app/src/main/java/com/example/orgo/sign_up.java
+++ app/src/main/java/com/example/orgo/sign_up.java
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
-    public void opensignup(View view){
-        Intent intent = new Intent( this, sign_up.class);
+    public void openlogin(View view){
+        Intent intent = new Intent( this,  MainActivity.class);
         startActivity(intent);
     }
 }
